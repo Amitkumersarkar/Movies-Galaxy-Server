d46eba991b31336c9d26617c186ce6d81c75239e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-09-10 14:01:57 +0600

    cilent side connect with server

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 4000;
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
-//to run in server
+// root route
 app.get('/', (req, res) => {
     res.send("Hello Developer");
-})
+});
 
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xqgbxlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -29,21 +28,34 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+        const movieCollection = client.db("movieDB").collection("movie");
+
+        // POST to add new movie
+        app.post('/movie', async (req, res) => {
+            try {
+                const newMovie = req.body;
+                const result = await movieCollection.insertOne(newMovie);
+                res.status(201).send(result);
+            } catch (err) {
+                res.status(500).send({ error: "Failed to add movie" });
+            }
+        });
+
+        // GET to fetch all movies
+        app.get('/movies', async (req, res) => {
+            const movies = await movieCollection.find().toArray();
+            res.send(movies);
+        });
+
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        console.log(" Connected to MongoDB!");
+    } catch (error) {
+        console.error(error);
     }
 }
 run().catch(console.dir);
 
-
-
-// to run server in default port
 app.listen(port, () => {
-    console.log(`Movies Galaxy Server Is Running On Port  : ${port}`);
-})
+    console.log(` Movies Galaxy Server running on port: ${port}`);
+});
